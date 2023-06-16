2ec5c170912904a09e088d523c2968b0a09ce20b
sergiofloresvt <sergioflores.vt@gmail.com>
2023-06-16 00:20:59 -0300

    segundo intento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarritoComponent } from './zapatuli/carrito/carrito.component';
+import { HomeComponent } from './zapatuli/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'home'},
+  {path:'home', component:HomeComponent},
+  {path:'carrito', component:CarritoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './zapatuli/header/header.component';
+import { ModelsComponent } from './zapatuli/models/models.component';
+import { HomeComponent } from './zapatuli/home/home.component';
+import { CarritoComponent } from './zapatuli/carrito/carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ModelsComponent,
+    HomeComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
